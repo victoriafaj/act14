64e95252cf92fa438c3c5f55b99ce41177a0be73
victoriafaj <84591393+victoriafaj@users.noreply.github.com>
2023-09-14 08:34:57 -0300

    Add files via upload

---
+++ act.js
@@ -0,0 +1,10 @@
+let boton1 = document.getElementById('b1')
+boton1.addEventListener('click', () => {
+    let boton1 = document.getElementById('b1')
+    alert(boton1.getAttribute('value'))
+})
+let boton2 = document.getElementById('b2')
+boton2.addEventListener('click', () => {
+    let boton2 = document.getElementById('b2')
+    alert(boton2.getAttribute('value'))
+})
